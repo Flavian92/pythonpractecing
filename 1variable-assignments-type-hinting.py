399afb3e086bb48ac57a87ebfa3426531cea7fb4
Flavian92 <flavianzefi@hotmail.com>
2023-11-06 11:29:00 +0100

    first exercise variables and type hint assig.

---
+++ 1variable-assignments-type-hinting.py
@@ -0,0 +1,60 @@
+var_1 = 10
+
+var_1 = 30.7  # changing value and type of var_1
+
+
+var_2 = 10.5
+var_3 = 10 + 10j
+name = "Flavian"
+var_5 = True  # ose False
+
+var_6 = None
+
+VAR_6 = "hello"  # DIFFERENT than var_6
+
+
+# print(var_1)
+# print(var_2, var_3)
+
+
+# type hinting (optional)
+a: int = 10
+
+a = 10.5
+# print(a)
+
+
+
+#explain part
+
+# var_1 = 10
+
+# var_1 is assigned the integer value 10.
+# var_1 = 30.7
+
+# var_1 is reassigned with the float value 30.7. This demonstrates that Python is dynamically typed, meaning a variable's type can change during its lifetime.
+# var_2 = 10.5
+
+# var_2 is assigned the float value 10.5.
+# var_3 = 10 + 10j
+
+# var_3 is assigned a complex number with a real part (10) and an imaginary part (10j).
+# name = "Flavian"
+
+# name is assigned the string value "damiano."
+# var_5 = True
+
+# var_5 is assigned the boolean value True. The comment suggests that it could also be assigned False, which is another boolean value.
+# var_6 = None
+
+# var_6 is assigned the special value None, which represents the absence of a value or a null value in Python.
+# VAR_6 = "hello"
+
+# VAR_6 is assigned the string value "hello." It's important to note that variable names in Python are case-sensitive, so VAR_6 and var_6 are different variables.
+# a: int = 10
+
+# This line uses type hinting to specify that the variable a should be of type int. It's assigned the integer value 10 in the next line.
+# a = 10.5
+
+# a is reassigned with the float value 10.5. This demonstrates that even with type hinting, Python still allows dynamic type changes. However, type hinting is a way to indicate the intended type of a variable, but it's not enforced by the interpreter.
+# Lastly, there are commented-out print statements, which, when uncommented and executed, would print the values of the variables. These statements are useful for checking the current values of the variables during program execution.
