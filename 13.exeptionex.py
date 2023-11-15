ada833de333aea140a3b3e3f7a019b355ed24913
Flavian92 <flavianzefi@hotmail.com>
2023-11-15 09:29:51 +0100

    exeption exercise upload

---
+++ 13.exeptionex.py
@@ -0,0 +1,24 @@
+# a = int(input("Vendosni vleren e a: "))
+# b = int(input("Vendosni vleren e b: "))
+
+# try:
+#     print(a / b)
+# except ZeroDivisionError:
+#     print("Pjestimi i gabuar, vlera e b eshte 0!")
+
+
+# l = [1, 2, 3, 4, 0]
+# try:
+#     print(l[0] / l[1])
+#     print(l[1] / l[2])
+#     print(l[2] / l[3])
+#     print(l[3] / l[4])
+#     print(l[4] / l[5])
+# except IndexError:
+#     print("Nuk ekziston elementi me indexin e dhene!")
+# except ZeroDivisionError:
+#     print("Pjestimi me 0 nuk eshte i mundur!")
+# except Exception as e:
+#     print(f"Ka ndodhur nje error! Errori eshte {e}")
+
+# print("Continuing the program...")
